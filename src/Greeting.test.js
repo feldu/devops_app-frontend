1af6ee978ade1bb982ee0d85426ff2226b165326
Egor Ostryakov <eg.ostr@mail.ru>
2024-09-27 17:33:40 +0300

    add more tests

--- src/Greeting.test.js
+++ src/Greeting.test.js
@@ -1,9 +1,24 @@
+// Greeting.test.js
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
+import '@testing-library/jest-dom';
 
-test('renders greeting message', () => {
-    render(<Greeting />);
-    const greetingElement = screen.getByText(/wroom-ru/i);
-    expect(greetingElement).toBeInTheDocument();
+describe('Greeting component', () => {
+    test('renders greeting message', () => {
+        render(<Greeting />);
+        const greetingElement = screen.getByText(/wroom-ru/i);
+        expect(greetingElement).toBeInTheDocument();
+    });
+
+    test('renders without crashing', () => {
+        const { container } = render(<Greeting />);
+        expect(container).toBeInTheDocument(); // Проверяем, что контейнер существует
+    });
+
+    test('does not render unexpected text', () => {
+        render(<Greeting />);
+        const unexpectedText = screen.queryByText(/hello/i); // Ищем текст, который не должен быть
+        expect(unexpectedText).not.toBeInTheDocument(); // Проверяем, что он не присутствует
+    });
 });
